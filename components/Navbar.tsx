42b72068b78e787a2189c513092b1ca171a3c30c
ovasquezbrito <ovasquezcyt@gmail.com>
2023-06-28 16:53:12 -0400

    feat: add constants

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
-
+import Link from 'next/link'
+import Image from 'next/image'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flexBetween navbar'>
+      <div className='flex-1 flexStart gap-10'>
+        <Link href="/">
+          <Image 
+            src="/logo.svg"
+            alt="Flexible"
+            width={115}
+            height={43}
+          />
+        </Link>
+      </div>
+    </nav>
   )
 }
 
