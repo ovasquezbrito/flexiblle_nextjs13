054ea1eec5b8d67c8198ec9916a744e6c4582e66
Oduber Vasquez <ovasquezcyt@gmail.com>
2023-06-28 21:35:43 -0400

    feat: add components/AuthProvider

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,45 @@
+import { footerLinks } from '@/constant'
+import Image from 'next/image'
+import Link from 'next/link';
 import React from 'react'
 
+type ColumnProps = {
+  title: string;
+  links: Array<string>;
+}
+const FooterColumn = ({title, links}:ColumnProps) => (
+  <div className='footer_column'>
+    <h4 className='font-semibold'>Title</h4>
+    <ul className='flex flex-col gap-2 font-normal'>
+      {links.map((link) => <Link href="/" key={link}>{link}</Link>)}
+    </ul>
+  </div>
+)
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='flexStart footer'>
+      <div className='flex flex-col gap-12 w-full'>
+        <div className='flex items-start flex-col'>
+          <Image 
+            src="/logo-purple.svg"
+            width={115}
+            height={38}
+            alt='Flexible'
+          />
+          <p className='text-start text-sm font-normal mt-5 max-w-xs'>
+            Flexible es la comunidad líder mundial para que los creativos compartan, crezcan y sean contratados
+          </p>
+        </div>
+        <div className='flex flex-wrap gap-12'>
+          <FooterColumn title={footerLinks[0].title} links={footerLinks[0].links}/>
+          <div className='flex-1 flex flex-col gap-4'>
+          <FooterColumn title={footerLinks[1].title} links={footerLinks[1].links}/>
+          <FooterColumn title={footerLinks[2].title} links={footerLinks[2].links}/>
+
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { NavLinks } from '@/constant'
+import {AuthProviders} from './'
 const Navbar = () => {
+  const session = null
   return (
     <nav className='flexBetween navbar'>
       <div className='flex-1 flexStart gap-10'>
@@ -12,6 +15,25 @@ const Navbar = () => {
             height={43}
           />
         </Link>
+        <ul className='xl:flex hidden text-small gap-7'>
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.key}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className='flexCenter gap-4'>
+        {session ? (
+          <>
+            UserPhoto
+            <Link href="/create-project">
+              Compartir trabajo
+            </Link>
+          </>
+        ):(
+          <AuthProviders />
+        )}
       </div>
     </nav>
   )
